5a89680660b55d9af13336ac0077b4de838ce32a
dkust <dan@cambr.io>
2020-01-23 16:12:46 -0500

    init from local

---
+++ LICENSE.md
@@ -0,0 +1,2 @@
+# Debug version for internal use only
+Cloned from https://github.com/DIUx-xView/xview2-baseline
---
+++ submission/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Debug version
+
+# Running inference using CLI arguments passed in
+# 1) path to xview2-baseline 
+# 2) path to input pre image 
+# 3) path to input post image 
+# 4) path to output localization image 
+# 5) path to output localization+classification image
+
+set -euxo
+
+echo "### BEGIN DEBUG ###"
+
+# DEBUG
+vmstat -s
+cat /proc/meminfo
+cat /proc/cpuinfo
+
+echo "### END DEBUG ###"
+
+if [ $# -lt 5 ]; then 
+        echo "run.sh: /path/to/xview2-baseline/ /path/to/input/pre/image /path/to/input/post/image /path/to/localization/output/image /path/to/classification/output/image" 
+else
+    "$1"/utils/inference.sh -x "$1" -i "$2"  -p "$3" -o "$4"  -l "$1"/weights/localization.h5 -c "$1"/weights/classification.hdf5 -y
+
+    echo "DEBUG list path args:"
+    ls -l $1
+    ls -l $2
+    ls -l $3
+    ls -l $4
+    ls -l $5
+    
+    # The two images we will use for scoring will be identical so just copying the output localization png to the classification path 
+    cp "$4" "$5"
+fi
